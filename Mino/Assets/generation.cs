0e03eb1fa09d9020e12f70bec5185a311e9b0d6a
findele <valennico@laposte.net>
2017-01-24 11:04:26 +0100

    First import of the project

---
+++ Mino/Assets/generation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generation : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject block;
+
+	// Use this for initialization
+	void Start() {
+		Instantiate (block, new Vector3(0, 0, 0), Quaternion.identity);
+		for(int i = 1; i <= 10; i++){
+			Instantiate (block, new Vector3(9.9f*i, 0, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Mino/Assets/heros.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heros : MonoBehaviour {
+
+	public float moveSpeed = 1f;
+	private float movement;
+	private float movement2;
+	public Rigidbody2D rg;
+
+	// Use this for initialization
+	void Start () {
+		rg = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		movement = Input.GetAxis("Vertical") * moveSpeed;
+		movement2 = Input.GetAxis("Horizontal") * moveSpeed;
+		movement *= Time.deltaTime;
+		movement2 *= Time.deltaTime;
+		//transform.Translate(movement2, movement, 0.0f);
+		rg.velocity = new Vector3 (movement2, movement, 0.0f);
+
+	}
+}
