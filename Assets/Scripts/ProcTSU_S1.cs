c6752db51d25d089cbcce5c077511965bf1384b4
Dieriga <laurent.gaidier@hotmail.fr>
2017-01-26 15:40:33 +0100

    Ajout scripts procedural projectiles haut bas

---
+++ Assets/Scripts/ProcTSU_S1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcTSU_S1 : MonoBehaviour
+{
+
+    /***************************
+     * Projectile lance tir en haut salle 1*
+     * *************************/
+
+    // declaration variables
+    int[,] GrilleMap = new int[20, 5]; // Tableau pour gestion salle1
+    int xPiege = 0;
+    int yPiege = 0;
+    int HautBas = 0;
+    int PiegePoseOk = 0;
+    [SerializeField]
+    GameObject throwSpearsUp;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Gestion emplacement alélatoire des lances
+        for (int i = 0; i <= 1; i++)
+        {
+
+            // Attribution des coordonnées           
+            xPiege = Random.Range(13, 19);
+            yPiege = Random.Range(2, 2);
+
+
+            // Creation du piege sur les coordonnées définis
+            // Si la place est déjà occupé, rien n'est placé
+            if (PiegePoseOk != 30)
+            {
+                if (GrilleMap[xPiege, yPiege] == 0)
+                {
+                    PiegePoseOk++;
+                    Vector3 pos0 = new Vector3(xPiege, yPiege, 0);
+                    Quaternion rot0 = Quaternion.identity;
+                    GameObject Lance0 = (GameObject)Instantiate(throwSpearsUp, pos0, rot0);
+                    GrilleMap[xPiege, yPiege] = PiegePoseOk;
+
+                }
+
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
+
+
